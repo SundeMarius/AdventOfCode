4ffdbbafcb44393a0cb18644bb5cacfa4f2c9f74
Marius Sunde Sivertsen <marius.sunde.sivertsen@protonmail.com>
2021-12-07 19:03:02 +0100

    2021: minor changes to comments

--- 2021/06/solution.py
+++ 2021/06/solution.py
@@ -5,10 +5,12 @@ import numpy as np
 lines = [int(fish) for fish in open(sys.argv[1]).read().strip('\n').split(',')]
 s0 = np.array([lines.count(f) for f in range(9)])
 
-""" Transition matrix 
-    1) Reduce number of days by 1 for each fish, add number of 0-fishes to number of 6-fishes.
-    2) Each 0-fish produces an 8-fish.
 """
+1) Reduce each fish by 1 day (left-shift)
+2) Add number of 0-fishes to number of 6-fishes
+3) Each 0-fish produces an 8-fish
+"""
+# Transition matrix
 M = np.matrix("0 1 0 0 0 0 0 0 0;"
               "0 0 1 0 0 0 0 0 0;"
               "0 0 0 1 0 0 0 0 0;"
@@ -18,6 +20,6 @@ M = np.matrix("0 1 0 0 0 0 0 0 0;"
               "1 0 0 0 0 0 0 1 0;"
               "0 0 0 0 0 0 0 0 1;"
               "1 0 0 0 0 0 0 0 0")
-# Solution is simply the sum of state vector components (#fishes)
-print("Number of fishes after 80 days: ", int(np.sum(M**80@s0)))
-print("Number of fishes after 256 days: ", int(np.sum(M**256@s0)))
+# Solution is simply the sum of state vector components after evolving state "d" days
+print("Number of fishes after 80 days: ", int(np.sum(M**80 @ s0)))
+print("Number of fishes after 256 days: ", int(np.sum(M**256 @ s0)))
