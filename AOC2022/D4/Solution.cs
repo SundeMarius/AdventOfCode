d0cc5471bbc5e88a41a2950df35b23221e53ff34
Marius Sunde Sivertsen <marius.sunde.sivertsen@protonmail.com>
2022-12-05 14:58:25 +0100

    Day four solutions

--- AOC2022/D4/Solution.cs
+++ AOC2022/D4/Solution.cs
@@ -5,17 +5,36 @@ internal class Solution
     public static string Sample => "D4/sample.txt";
     public static string Input => "D4/input.txt";
 
-    public Solution(string fileInput)
-    {
-    }
+    private readonly List<string> _assignments = new();
+
+    public Solution(string fileInput) => _assignments.AddRange(from assignment in File.ReadLines(fileInput)
+                                                               select assignment);
 
     public int SolvePart1()
     {
-        return 0;
+        int total = 0;
+        foreach(var assignment in _assignments)
+        {
+            var leftId1 = int.Parse(assignment.Split(',')[0].Split('-')[0]);
+            var leftId2 = int.Parse(assignment.Split(',')[0].Split('-')[1]);
+            var rightId1 = int.Parse(assignment.Split(',')[1].Split('-')[0]);
+            var rightId2 = int.Parse(assignment.Split(',')[1].Split('-')[1]);
+            total += (leftId1 <= rightId1 && leftId2 >= rightId2) || (rightId1 <= leftId1 && rightId2 >= leftId2) ? 1 : 0;
+        }
+        return total;
     }
     
     public int SolvePart2()
     {
-        return 0;
+        int total = 0;
+        foreach(var assignment in _assignments)
+        {
+            var leftId1 = int.Parse(assignment.Split(',')[0].Split('-')[0]);
+            var leftId2 = int.Parse(assignment.Split(',')[0].Split('-')[1]);
+            var rightId1 = int.Parse(assignment.Split(',')[1].Split('-')[0]);
+            var rightId2 = int.Parse(assignment.Split(',')[1].Split('-')[1]);
+            total += leftId2 < rightId1 || rightId2 < leftId1 ? 0 : 1;
+        }
+        return total;
     }
 }
--- AOC2022/Program.cs
+++ AOC2022/Program.cs
@@ -1,4 +1,4 @@
-﻿using AOC2022.D3;
+﻿using AOC2022.D4;
 
 var solutionSample = new Solution(Solution.Sample);
 var solutionInput = new Solution(Solution.Input);
