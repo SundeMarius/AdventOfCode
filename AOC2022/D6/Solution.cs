598e890a1154e9306eaf459909bebf0004373e0b
Marius Sunde Sivertsen <mass@dips.no>
2022-12-06 15:58:54 +0100

    Day six solutions

--- AOC2022/D6/Solution.cs
+++ AOC2022/D6/Solution.cs
@@ -5,18 +5,33 @@ internal class Solution
     public static string Sample => "D6/sample.txt";
     public static string Input => "D6/input.txt";
 
+    private readonly char[] _chars;
+
     public Solution(string fileInput)
     {
-        
+        _chars = File.ReadAllText(fileInput).ToCharArray();
     }
-        
+
+    private int GetStartOfPacketCount(int capacity)
+    {
+        var index = 0;
+        HashSet<char> window = new(capacity);
+        while (window.Count != capacity)
+        {
+            window.Clear();
+            for (var i = 0; i < capacity; i++) window.Add(_chars[index+i]);
+            index++;
+        }
+        return index+capacity-1;
+    }
+
     public int SolvePart1()
     {
-        return 0;
+        return GetStartOfPacketCount(4);
     }
-    
+
     public int SolvePart2()
     {
-        return 0;
+        return GetStartOfPacketCount(14);
     }
 }
--- AOC2022/Program.cs
+++ AOC2022/Program.cs
@@ -1,4 +1,4 @@
-﻿using AOC2022.D5;
+﻿using AOC2022.D6;
 
 var solutionSample = new Solution(Solution.Sample);
 var solutionInput = new Solution(Solution.Input);
